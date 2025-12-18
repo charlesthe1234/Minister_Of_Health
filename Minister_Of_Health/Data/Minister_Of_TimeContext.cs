5056d17c7a6479947c18e41ceb9cb6bc43a93fd0
HengJun <tanhengjun@gmail.com>
2025-12-18 10:01:57 +0800

    Working pages/ entities

--- Minister_Of_Health/Data/Minister_Of_TimeContext.cs
+++ Minister_Of_Health/Data/Minister_Of_TimeContext.cs
@@ -21,5 +21,31 @@ namespace Minister_Of_Time.Data
         public DbSet<Minister_Of_Time.Domain.Activity> Activity { get; set; } = default!;
         public DbSet<Minister_Of_Time.Domain.CloseConnection> CloseConnection { get; set; } = default!;
         public DbSet<Minister_Of_Time.Domain.StressRecord> StressRecord { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // 1. Fix CloseConnection (Multiple paths to User)
+            modelBuilder.Entity<CloseConnection>(entity => {
+                entity.HasOne(d => d.User).WithMany().HasForeignKey(d => d.UserId).OnDelete(DeleteBehavior.Restrict);
+                entity.HasOne(d => d.ConnectedUser).WithMany().HasForeignKey(d => d.ConnectedUserId).OnDelete(DeleteBehavior.Restrict);
+            });
+
+            // 2. Fix CalendarEvent (HostUser loop)
+            modelBuilder.Entity<CalendarEvent>(entity => {
+                entity.HasOne(d => d.HostUser).WithMany().HasForeignKey(d => d.HostUserId).OnDelete(DeleteBehavior.Restrict);
+            });
+
+            // 3. Fix EventParticipant (The final cycle error you saw)
+            modelBuilder.Entity<EventParticipant>(entity => {
+                entity.HasOne(d => d.User).WithMany().HasForeignKey(d => d.UserId).OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<StressRecord>(entity =>
+            {
+                entity.HasOne(d => d.User).WithMany().HasForeignKey(d => d.UserId).OnDelete(DeleteBehavior.Restrict);
+            });
+
+        }
     }
 }
