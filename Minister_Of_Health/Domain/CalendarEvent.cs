be85d3b587ee13f9df16149cab1801fc7b45d405
HengJun <tanhengjun@gmail.com>
2026-01-18 04:23:48 +0800

    Touched up on Ui to show the 6 colours (currently not working), and updated manage code to add reinvite feature.

--- Minister_Of_Health/Domain/CalendarEvent.cs
+++ Minister_Of_Health/Domain/CalendarEvent.cs
@@ -1,24 +1,25 @@
-﻿using System.Diagnostics;
-using Minister_Of_Time.Data;
+﻿using Minister_Of_Time.Data;
+using System.Collections.Generic;
+
 namespace Minister_Of_Time.Domain
 {
     public class CalendarEvent : BaseDomainModel
     {
-
         public string EventName { get; set; } = string.Empty;
         public string? Location { get; set; }
         public DateTime StartDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
         public string? CalendarType { get; set; }
-        public string? Status { get; set; }
+        public string Status { get; set; } = "Upcoming"; // Default status
         public string? Description { get; set; }
 
         // Foreign Keys 
-        public String HostUserId { get; set; }
+        public string HostUserId { get; set; }
         public int UserActivityId { get; set; }
 
         // Navigation Properties
-        public virtual User? HostUser { get; set; } // Link back to the User who is hosting
+        public virtual User? HostUser { get; set; }
         public virtual UserActivity? UserActivity { get; set; }
+        public virtual ICollection<EventParticipant> EventParticipant { get; set; } = new List<EventParticipant>();
     }
 }
