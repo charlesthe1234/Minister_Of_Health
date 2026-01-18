e6d62a2769a9689a2506f64593ad1f8333411212
charlesthe1234 <mhr2133226041@gmail.com>
2026-01-18 22:41:06 +0800

    Api

---
+++ Minister_Of_Health/Controllers/ActivityTypesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Minister_Of_Time.Data;
+using Minister_Of_Time.Domain;
+
+namespace Minister_Of_Time.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityTypesController : ControllerBase
+    {
+        private readonly Minister_Of_TimeContext _context;
+
+        public ActivityTypesController(Minister_Of_TimeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ActivityTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ActivityType>>> GetActivityType()
+        {
+            return await _context.ActivityType.ToListAsync();
+        }
+
+        // GET: api/ActivityTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ActivityType>> GetActivityType(int id)
+        {
+            var activityType = await _context.ActivityType.FindAsync(id);
+
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            return activityType;
+        }
+
+        // PUT: api/ActivityTypes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutActivityType(int id, ActivityType activityType)
+        {
+            if (id != activityType.ActivityTypeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(activityType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActivityTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ActivityTypes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ActivityType>> PostActivityType(ActivityType activityType)
+        {
+            _context.ActivityType.Add(activityType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetActivityType", new { id = activityType.ActivityTypeId }, activityType);
+        }
+
+        // DELETE: api/ActivityTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivityType(int id)
+        {
+            var activityType = await _context.ActivityType.FindAsync(id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            _context.ActivityType.Remove(activityType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ActivityTypeExists(int id)
+        {
+            return _context.ActivityType.Any(e => e.ActivityTypeId == id);
+        }
+    }
+}
